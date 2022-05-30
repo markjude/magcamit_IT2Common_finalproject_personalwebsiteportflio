a27c6f8b9ed5331401b111d01ee1246f60ba051e
markjude <60818626+markjude@users.noreply.github.com>
2022-05-30 17:38:00 +1000

    Add files via upload
    front

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="a">
+        <h1>Personal Website Portfolio</h1>
+        <div>
+            <table cellspacing="40">
+                <tr>
+                    <td><a style="margin-left: -20%;" href="./Team Song/song.php"><img src="music.png" class="music" alt=""></a></td>
+                    <td><a style="margin-left: 5%;" href="./Team Morse Code/code.php"><img src="2.png" class="morse" alt=""></a></td>
+                    <td><a style="margin-left: 20%;" href="./Team Zodiac/zodiac.php"><img src="signs.jpg" class="zodiac" alt=""></a></td>
+                </tr>
+                <tr>
+                    <td><h4 style="margin-left: 10%;">My Songs Choice</h4></td>
+                    <td><h4 style="margin-left: 30%;">Morse Code</h4></td>
+                    <td><h4 style="margin-left: 55%;">Birth Stones & Zodiac Sign</h4></td>
+                </tr>
+                <tr>
+                  
+                    <td><a style="margin-left: -20%;" href="./Team Investment/index.php"><img src="investment.jpg" class="vest" alt=""></a></td>
+                    <td><a  style="margin-left: 5%;" href="./Profile/self.php"><img src="elgn.jpg" class="pogi" alt=""></a></td>
+                    <td><a  style="margin-left: 20%;" href="./Profile 2/self2.php"><img src="malakas.jpg" class="jude" alt=""></a></td>
+                </tr>
+                <tr>
+                    <td><h4 style="margin-left: 10%;">Future  Investments</h4></td>
+                    <td><h4 style="margin-left: 30%;">Elgen's Personal information</h4></td>
+                    <td><h4 style="margin-left: 55%;">Jude's Personal Information</h4></td>
+                </tr>
+                <tr>
+                <td><h2 style="margin-left: -20%;">Let's Socialize</h2></td>
+                </tr>
+                <tr>
+                <td><a style="margin-left: 60%;" href="https://www.facebook.com/markjude.magcamit"><img src="assets/img/facebook.png" class="fb" alt=""></a></td>
+                <td><a style="margin-left: 90%;" href="https://www.linkedin.com"><img src="assets/img/linkedin.png" class="link" alt=""></a></td>
+                </tr>
+                <tr>
+                <td><h4 style="margin-left: 70%;">FACEBOOK</h4></td>
+                <td><h4 style="margin-left: 100%;">LINKEDIN</h4></td>
+                </tr>
+                
+            </table>
+        </div>
+    <div class="b">
+        <footer>
+            <br>
+            <br>
+            <label>Created by Magcamit Mark Jude & Mago Elgen</label>
+        </footer>
+    </div>
+    </div>
+</body>
+</html>
