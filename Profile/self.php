c4f6f814fd684d1aa00773fb8e44d816f317b87f
markjude <60818626+markjude@users.noreply.github.com>
2022-05-30 17:42:24 +1000

    Add files via upload
    elgen info

---
+++ Profile/self.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ELGEN'S Info
+    </title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class = "me-myself-container">
+    <h1 class = "title">PERSONAL INFO</h1>
+    <div class = "container">
+    <?php
+    $a = array("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
+    $b = array("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S",'T',"U","V","W","X","Y","Z");
+    $c = array("0","1","2","3","4","5","6","7","8","9");
+
+
+  ?>
+  <div>
+  <img src="img/received_4413615141997248.jpeg" class= "picture" alt="">
+  </div> 
+  <?php
+   echo "<p> Full Name: ". $b[4].$a[11].$a[6].$a[4].$a[13]. ". ".$b[12].". ".$b[12].$a[0].$a[6].$a[14]."</p>";
+   echo "<p> Address: ". $b[1].$a[20].$a[13].$a[6].$a[0].$a[13].$a[0].$a[24]. ",".$b[1].$a[14],$a[0].$a[2]. ",". $b[12].$a[0].$a[17].$a[8].$a[13].$a[3].$a[20].$a[16].$a[20].$a[4]."</p>";
+   echo "<p> Age: ". $c[2].$c[1]."</p>";
+   echo "<p> Birthday: ". $b[18].$a[4].$a[15].$a[19].$a[4].$a[12].$a[1].$a[4].$a[17]. " ".$c[2].$c[7]. ", ".$c[1].$c[9],$c[9].$c[9]."</p>";
+   echo "<p> Gender: ". $b[12].$a[0].$a[11].$a[4]."</p>";
+   echo "<p> Height: ". $c[1].$c[7].$c[5]."cm</p>";
+   echo "<p> Weight: ". $c[5].$c[5]." ".$c[5]."kg</p>";
+   echo "<p> Self-Interest: ". $b[8]." ".$a[11].$a[14].$a[21].$a[4].  " ".$a[15].$a[11].$a[0].$a[24].$a[8].$a[13].$a[6]. " ".$a[1].$a[0].$a[18].$a[10].$a[4].$a[17].$a[1].$a[0].$a[11].$a[11]. " ".$a[18].$a[15].$a[14].$a[17].$a[19].$a[18]. ". ".$b[8]. " ".$a[18].$a[15].$a[4].$a[13].$a[3]. " ".$a[12].$a[24]. " ".$a[19].$a[8].$a[12].$a[4]. " ".$a[8].$a[13]. " ".$a[22].$a[0].$a[19].$a[2].$a[7].$a[8].$a[13].$a[6]. " ".$a[0].$a[13].$a[8].$a[12].$a[4]. " ".$a[0].$a[13].$a[3]. " ".$a[12].$a[14].$a[21].$a[8].$a[4].$a[18]. ". ".$b[8]. " ".$a[11].$a[8].$a[10].$a[4]. " ".$a[0].$a[11].$a[18].$a[14]. " ".$a[19].$a[14]. " ".$a[6].$a[14]. " ".$a[19].$a[14]. " ".$a[3].$a[8].$a[5].$a[5].$a[4].$a[17].$a[4].$a[13].$a[19]. " ".$a[10].$a[8].$a[13].$a[3].$a[18]. " ".$a[14].$a[5]. " ".$a[15].$a[11].$a[0].$a[2].$a[4].$a[18]. " ".$a[19].$a[14]. " ".$a[3].$a[8].$a[18].$a[2].$a[14].$a[21].$a[4].$a[17]."</p>";
+ ?>
+ <div>
+ 
+ </div>
+ <div class = "educ">
+ <h1 class title>Educational Background</h1>
+ <div>
+ <?php
+
+echo "<p> Elementary School: ". $b[2].$a[0].$a[22].$a[8].$a[19]. ", ".$b[4].$a[11].$a[4].$a[12].$a[4].$a[13].$a[19].$a[0].$a[17].$a[24]. ", ".$b[18].$a[2].$a[7].$a[14].$a[14].$a[11]."</p>";
+echo "<p> High School: ". $b[2].$a[0].$a[22].$a[8].$a[19]. ", ".$b[7].$a[8].$a[6].$a[7].", ".$b[18].$a[2].$a[7].$a[14].$a[14].$a[11]. "</p>";
+echo "<p> College: ".$b[12].$a[0].$a[17].$a[8].$a[13].$a[3].$a[20].$a[16].$a[20].$a[4]. " ".$b[18].$a[19].$a[0].$a[19].$a[4]. " ".$b[2].$a[14].$a[11].$a[11].$a[4].$a[6].$a[4]. "</p>";
+?>
+
+    </div>
+    
+    <div class = "Movies">
+    <h1 class title> My Favorite Movies</h1>
+    <img src="img/download (2).jpeg" class = "pic1" alt="">
+    <img src="img/download (3).jpeg" class = "pic2" alt="">
+    <img src="img/download (4).jpeg" class = "pic3" alt="">
+    <img src="img/download (5).jpeg" class = "pic4" alt="">
+    </div>
+   
+</body>
+</html>
