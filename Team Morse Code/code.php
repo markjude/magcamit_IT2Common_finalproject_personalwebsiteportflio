6ec906f5b47bc121e91a88400be706e778ea1eda
markjude <60818626+markjude@users.noreply.github.com>
2022-05-30 17:44:18 +1000

    Add files via upload
    morsecode icon

---
+++ Team Morse Code/code.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div>
+		
+		<?php
+		echo "<h1 align='center'>International Morse Code</h1>";
+	echo "<table cellspacing='10'>
+		<tr>
+			<td><h1>A</h1></td>
+			<td align='center'><p class='circle'></p></td>
+            <td><p class='rectangle'></p></td>
+            <td></td>
+            <td></td>
+            <td></td>
+
+
+            <td><h1>N</h1></td>
+			<td><p class='rectangle'></p></td>
+			<td align='center><p class='circle'></p></td>
+			</tr>
+		<tr>
+			<td><h1>B</h1></td>
+			<td><p class='rectangle'></p></td>
+			<td align='center'><p class='circle'></p></td>
+			<td align='center'><p class='circle'></p></td>
+            <td align='center'><p class='circle'></p></td>
+            <td></td>
+            <td><h1>O</h1></td>
+			<td><p class='rectangle'></p></td>
+			<td><p class='rectangle'></p></td>
+			<td><p class='rectangle'></p></td>
+			</tr>
+		<tr>
+			<td><h1>C</h1></td>
+			<td><p class='rectangle'></p></td>
+			<td align='center'><p class='circle'></p></td>
+			<td><p class='rectangle'></p></td>
+            <td align='center'><p class='circle'></p></td>
+            <td></td>
+            <td><h1>P</h1></td>
+			<td align='center'><p class='circle'></p></td>
+			<td><p class='rectangle'></p></td>
+			<td><p class='rectangle'></p></td>
+			<td align='center'><p class='circle'></p></td>
+        </tr>
+            <td><h1>D</h1></td>
+			<td><p class='rectangle'></p></td>
+			<td align='center'><p class='circle'></p></td>
+            <td align='center'><p class='circle'></p></td>
+            <td></td>
+            <td></td>
+            <td><h1>Q</h1></td>
+			<td><p class='rectangle'></p></td>
+			<td><p class='rectangle'></p></td>
+			<td align='center'><p class='circle'></p></td>
+			<td><p class='rectangle'></p></td>
+        </tr>
+
+        <tr>
+			<td><h1>E</h1></td>
+            <td align='center'><p class='circle'></p></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>   
+            <td><h1>R</h1></td>
+			<td align='center'><p class='circle'></p></td>
+			<td><p class='rectangle'></p></td>
+			<td align='center'><p class='circle'></p></td>
+        </tr>   
+        <tr>     
+            <td><h1>F</h1></td>
+			<td align='center'><p class='circle'></p></td>
+			<td align='center'><p class='circle'></p></td>
+            <td><p class='rectangle'></p></td>
+            <td></td>
+            <td></td>
+            <td><h1>S</h1></td>
+			<td align='center'><p class='circle'></p></td>
+			<td align='center'><p class='circle'></p></td>
+			<td align='center'><p class='circle'></p></td>
+        </tr>
+        <tr>
+			<td><h1>G</h1></td>
+			<td><p class='rectangle'></p></td>
+			<td><p class='rectangle'></p></td>
+            <td align='center'><p class='circle'></p></td>
+            <td></td>
+            <td></td>
+            <td><h1>T</h1></td>
+			<td><p class='rectangle'></p></td>
+        </tr>
+        <tr>
+			<td><h1>H</h1></td>
+			<td align='center'><p class='circle'></p></td>
+			<td align='center'><p class='circle'></p></td>
+			<td align='center'><p class='circle'></p></td>
+            <td align='center'><p class='circle'></p></td>
+            <td></td>
+            <td><h1>U</h1></td>
+			<td align'center'><p class='circle'></p></td>
+			<td><p class='rectangle'></p></td>
+		</tr>
+        <tr>
+			<td><h1>I</h1></td>
+			<td align='center'><p class='circle'></p></td>
+            <td align='center'><p class='circle'></p></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td><h1>V</h1></td>
+			<td align='center'><p class='circle'></p></td>
+			<td align='center'><p class='circle'></p></td>
+			<td align='center'><p class='circle'></p></td>
+			<td><p class='rectangle'></p></td>
+        </tr>
+        <tr>
+			<td><h1>J</h1></td>
+			<td align='center'><p class='circle'></p></td>
+			<td><p class='rectangle'></p></td>
+			<td><p class='rectangle'></p></td>
+            <td><p class='rectangle'></p></td>
+            <td></td>
+            <td><h1>W</h1></td>
+			<td align='center'><p class='circle'></p></td>
+			<td><p class='rectangle'></p></td>
+			<td><p class='rectangle'></p></td>
+        </tr>
+        <tr>
+			<td><h1>K</h1></td>
+			<td><p class='rectangle'></p></td>
+			<td align='center'><p class='circle'></p></td>
+            <td><p class='rectangle'></p></td>
+            <td></td>
+            <td></td>
+            <td><h1>X</h1></td>
+			<td><p class='rectangle'></p></td>
+			<td align='center'><p class='circle'></p></td>
+			<td align='center'><p class='circle'></p></td>
+			<td><p class='rectangle'></p></td>
+        </tr>
+        <tr>
+			<td><h1>L</h1></td>
+			<td align='center'><p class='circle'></p></td>
+			<td><p class='rectangle'></p></td>
+			<td align='center'><p class='circle'></p></td>
+            <td align='center'><p class='circle'></p></td>
+            <td></td>
+            <td><h1>Y</h1></td>
+			<td><p class='rectangle'></p></td>
+			<td align='center'><p class='circle'></p></td>
+			<td><p class='rectangle'></p></td>
+			<td><p class='rectangle'></p></td>
+        </tr>
+        <tr>
+			<td><h1>M</h1></td>
+			<td><p class='rectangle'></p></td>
+            <td><p class='rectangle'></p></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td><h1>Zz</h1></td>
+			<td><p class='rectangle'></p></td>
+			<td><p class='rectangle'></p></td>
+			<td align='center'><p class='circle'></p></td>
+			<td align='center'><p class='circle'></p></td>
+		</tr>
+
+	</table>";
+	?>
+</body>
+</html>
